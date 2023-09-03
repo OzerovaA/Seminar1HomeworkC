a2e6edda56d6971ba364e574d5d94a9665dd3386
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-09-03 18:15:08 +0300

    Добавлено решение задания 8

---
+++ task8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+if (N >0)
+{
+    while (count <= N)
+    { if(count%2 == 0) 
+        {
+            Console.Write(count +" ");
+            count++;
+        }
+        else 
+        {
+            count++;
+        }
+
+    }
+}
+else 
+{
+    Console.WriteLine("Программа работает только с положительными числами!");
+}
