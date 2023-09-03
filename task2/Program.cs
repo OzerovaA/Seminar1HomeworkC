cc947b9eab88aa7a53284db3cfa897b683c49eb5
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-09-03 17:57:32 +0300

    Добавлено решение задания 2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + "Больше чем "+number2);
+}
+else 
+{
+    Console.WriteLine(number2 + " Больше чем "+number1);
+}
