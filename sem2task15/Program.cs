349634906d5b60c1245cc8e610a009af183736f7
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-10-07 17:59:13 +0300

    Добавлено задание 15 к дз семинара 2

---
+++ sem2task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number>0 & number<8)
+{
+    if (number==6 | number==7)
+    {
+        Console.WriteLine("День недели - выходной");
+    }
+    else {Console.WriteLine("День недели - не выходной");}
+}
+else {Console.WriteLine("Такого дня недели не существует!");}
