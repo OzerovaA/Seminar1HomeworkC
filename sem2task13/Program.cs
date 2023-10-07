0bacb6b66e588c2929278699c325a0afc4196638
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-10-07 17:51:44 +0300

    Добавлено задание 13 к дз семинара 2

---
+++ sem2task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number>=100)
+{
+    while (number>999)
+    {
+        number = number/10;
+    }
+
+Console.WriteLine(number%10);
+}
+else if (number <100)
+{Console.WriteLine("Третьей цифры нет");}
+
