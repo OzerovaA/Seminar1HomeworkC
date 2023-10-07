9407818cf85fdd3521d57f6c9abdcd7b58836682
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-10-07 20:44:17 +0300

    Добавлено sem3task1

---
+++ sem3task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      if (number>9999 & number<100000)
+      {
+        int n = number;
+        int reversed = 0;
+        while (n>0)
+        {
+            int last = n%10;
+            reversed=reversed*10+last;
+            n=n/10;
+        }
+        return reversed==number;
+      }
+      else {
+        Console.WriteLine("Число не пятизначное");
+        return false;
+        }
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 645467;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
